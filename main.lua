dd5cc77a73e51c6c8a9e51c0023f7a55838175e0
Peter Erdos <erdospepe9@gmail.com>
2025-07-11 12:02:44 +0200

    Enable console output in configuration and integrate card functionality in main logic

--- main.lua
+++ main.lua
@@ -1,17 +1,23 @@
 local class = require 'middleclass'
 local SceneManager = require 'scene_manager'
 --local Player = require 'player'
---local Card = require 'card'
+local Card = require 'card'
 --local Scene = require 'scene'
 
 local scene_manager = SceneManager:new()
+local card = Card:new()
 
 function love.load()
-    
 end
+
+function love.update(dt)
+    print("current scene:", scene_manager.current_scene)    
+end
+
 
 function love.draw()
     scene_manager:draw()
+    card:draw()
 end
 
 function love.mousepressed(x, y, button)
